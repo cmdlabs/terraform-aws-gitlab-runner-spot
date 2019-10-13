c065c442e9d002eb08ce874674096940d956955a
Alex Harvey <alex.harvey@cmd.com.au>
2019-10-13 15:57:39 +1100

    Remove enable_cloudwatch_logging feature

--- logging.tf
+++ logging.tf
@@ -1,16 +1,13 @@
 data "template_file" "instance_profile" {
-  count    = var.enable_cloudwatch_logging ? 1 : 0
   template = file("${path.module}/policies/instance-logging-policy.json")
 }
 
 resource "aws_iam_role_policy" "instance" {
-  count  = var.enable_cloudwatch_logging ? 1 : 0
   name   = "gitlab-runner-instance-role"
   role   = aws_iam_role.instance.name
-  policy = data.template_file.instance_profile[0].rendered
+  policy = data.template_file.instance_profile.rendered
 }
 
 resource "aws_cloudwatch_log_group" "environment" {
-  count = var.enable_cloudwatch_logging ? 1 : 0
   name  = "gitlab-runner-log-group"
 }
