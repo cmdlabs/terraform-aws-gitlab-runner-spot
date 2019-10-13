e3e91d8d3f2b57a4d19bde29c057f38f753e9d43
Alex Harvey <alex.harvey@cmd.com.au>
2019-10-13 15:58:10 +1100

    Move logging.tf into main.tf

--- main.tf
+++ main.tf
@@ -445,3 +445,16 @@ resource "aws_iam_role_policy_attachment" "ssm" {
   role       = aws_iam_role.instance.name
   policy_arn = aws_iam_policy.ssm[0].arn
 }
+data "template_file" "instance_profile" {
+  template = file("${path.module}/policies/instance-logging-policy.json")
+}
+
+resource "aws_iam_role_policy" "instance" {
+  name   = "gitlab-runner-instance-role"
+  role   = aws_iam_role.instance.name
+  policy = data.template_file.instance_profile.rendered
+}
+
+resource "aws_cloudwatch_log_group" "environment" {
+  name  = "gitlab-runner-log-group"
+}
